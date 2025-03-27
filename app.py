03d94308d75b2b94dfbabed96dba350a8b9a1248
Vishwak Senan Ganesan <vishwak-senan.ganesan@lloydsbanking.dev>
2025-03-27 11:16:22 +0000

    test

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def feature2():
 
 @app.route('/')
 def hello():
-	return "Test from Demo PoC\n"
+	return "Test from Demo Special Day\n"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
